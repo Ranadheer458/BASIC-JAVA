ebe788e7cd8ce62a656dce03b544b5c7c2ef87ad
Ranadheer Powdapally <ranadheer@hooperlabs.com>
2021-04-04 23:57:37 +0530

    added try catch for division

--- src/calculator/basic.java
+++ src/calculator/basic.java
@@ -19,14 +19,22 @@ public class basic implements Icalculator  {
 	@Override
 	public void division(int a, int b) {
 		// TODO Auto-generated method stub
-		int res = a/b;
-		System.out.println("division: "+res);	
+		try {
+			int res = a/b;
+			System.out.println("division: "+res);
+			}
+			catch(Exception e)
+			{
+			System.out.println("division is not posible by zero");
+			}	
 	}
 
 	@Override
 	public void multiplication(int a, int b) {
 		// TODO Auto-generated method stub
+		
 		int res = a*b;
 		System.out.println("multiplication: "+res);
-	}
+		}
+		
 }
