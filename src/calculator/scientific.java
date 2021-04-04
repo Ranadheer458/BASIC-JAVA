f0923700f55ff624a0972fb905ddc137bde85e23
Ranadheer Powdapally <ranadheer@hooperlabs.com>
2021-04-05 00:29:39 +0530

    try catch added for modulus

--- src/calculator/scientific.java
+++ src/calculator/scientific.java
@@ -1,11 +1,13 @@
 package calculator;
 
-public class scientific extends basic{
-	
-	public void modulus(int a,int b)
-	{
-	int res = a%b;
-	System.out.println("Modulus: "+res);
-	}
+public class scientific extends basic {
 
+	public void modulus(int a, int b) {
+		try {
+			int res = a % b;
+			System.out.println("Modulus: " + res);
+		} catch (Exception e) {
+			System.out.println("modulus is not possible with zero");
+		}
+	}
 }
